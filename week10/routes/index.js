a8e0a622152a4e6997b812fb3832e7a6bdab87a8
miq28 <iqbal.tiro@gmail.com>
2021-12-18 19:04:38 +0700

    refactor

--- week10/routes/index.js
+++ week10/routes/index.js
@@ -1,7 +1,5 @@
 // routes/index.js
 
-const auth = require('../auth/auth.jwt')
-
 module.exports = app => {
     require("./other.routes")(app);
     require("./province.routes")(app);
@@ -10,27 +8,3 @@ module.exports = app => {
     require("./user.routes")(app);
     require("./office.routes")(app);
 };
-
-// module.exports = app => {
-//     const root = require("../controllers");
-
-//     var router = require("express").Router();
-
-//     router.get('/protected', auth, root.protected);
-
-//     app.use('/', router);
-// };
-
-// require("./routes")(app);
-// require("./province.routes")(app);
-// require("./regency.routes")(app);
-// require("./district.routes")(app);
-// require("./user.routes")(app);
-// require("./office.routes")(app);
-
-// require("./routes")(app);
-// require("./routes/province.routes")(app);
-// require("./routes/regency.routes")(app);
-// require("./routes/district.routes")(app);
-// require("./routes/user.routes")(app);
-// require("./routes/office.routes")(app);
